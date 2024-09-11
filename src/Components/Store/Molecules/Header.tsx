265f500469ed208c98c368dddf696c1a3a7bc1ac
AbhijithKK <abhijithkkpurameri@gmail.com>
2024-09-11 12:21:57 +0530

    update headder style

--- src/Components/Store/Molecules/Header.tsx
+++ src/Components/Store/Molecules/Header.tsx
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import '../Helpers/scss/Headder.scss'
 import profile from "../../../assets/blank-profile-picture-973460_1280.png"
 import logo from '../../../assets/favicon.ico'
 import { BsCart } from "react-icons/bs";
 const Header: React.FC = () => {
+    const [isDropdownOpen, setIsDropdownOpen] = useState<boolean>(false);
+  const [profileName] = useState("John Doe")
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
+
   return (
     <header className="header">
       {/* Left side: Logo and Store Name */}
@@ -27,17 +33,29 @@ const Header: React.FC = () => {
 
       {/* Right side: Cart, Profile, Login */}
       <div className="header__right">
+        {/* Cart Icon */}
         <div className="header__icon header__cart">
-        <BsCart />        </div>
-        <div className="header__profile">
+          <BsCart />
+        </div>
+
+        {/* Profile Section */}
+        <div className="header__profile" onClick={toggleDropdown}>
           <img
             src={profile}
             alt="Profile"
             className="header__profile-pic"
           />
-        </div>
-        <div className="header__login">
-          <a href="/login">Login</a>
+          <div className="header__profile-name">{profileName}</div>
+
+          {/* Dropdown menu */}
+          {isDropdownOpen && (
+            <div className="header__dropdown">
+              <ul>
+                <li><a href="/settings">Settings</a></li>
+                <li><a href="/logout">Logout</a></li>
+              </ul>
+            </div>
+          )}
         </div>
       </div>
     </header>
