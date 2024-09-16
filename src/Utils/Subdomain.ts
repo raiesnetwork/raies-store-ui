436b077f31cd4a12a7231a140d5333698a3f4158
AbhijithKK <abhijithkkpurameri@gmail.com>
2024-09-16 22:02:08 +0530

    change subdomain get function

--- src/Utils/Subdomain.ts
+++ src/Utils/Subdomain.ts
@@ -1,11 +1,9 @@
-export function getSubdomain(url: string | URL) {
-  const { hostname } = new URL(url);
-
-  const parts = hostname.split(".");
-
-  if (parts.length > 2) {
-    return parts.slice(0, parts.length - 2).join(".");
+export function getSubdomain(hostname: string) {
+    const parts = hostname.split(".");
+  
+    if (parts.length > 2) {
+      return parts.slice(0, parts.length - 2).join(".");
+    }
+  
+    return null; 
   }
-
-  return null;
-}
