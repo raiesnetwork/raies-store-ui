4a821badd0e51db99aebe84e7b5c0bbb6868337f
AbhijithKK <abhijithkkpurameri@gmail.com>
2024-10-14 14:47:36 +0530

    remove unwanted codes in product card

--- src/Components/Store/Molecules/ProductCard.tsx
+++ src/Components/Store/Molecules/ProductCard.tsx
@@ -1,36 +1,14 @@
 import React from "react";
 import "../Helpers/scss/ProductCard.scss";
-import useMystoreStore from "../Core/Store";
-import { ProductViewCardProps, respProduct } from "../Core/Interfaces";
+import { ProductViewCardProps } from "../Core/Interfaces";
 import { Link } from "react-router-dom";
 import { LiaExchangeAltSolid } from "react-icons/lia";
-// import { RiAuctionLine } from "react-icons/ri";
-// import { TbMoneybag } from "react-icons/tb";
+
 import { GiReceiveMoney } from "react-icons/gi";
 
 const ProductViewCard: React.FC<ProductViewCardProps> = ({ data }) => {
-  const { updateSingleProductData } = useMystoreStore((state) => state);
-  const handleDetaildView = (data: respProduct) => {
-    updateSingleProductData(data);
-  };
-  // const [btnDisable, setDisable] = useState<boolean>(false)
-  // const handileCart = async (id: string, count: number, userId: string) => {
-  //   if (logedIn === true) {
-  //     setDisable(true)
-
-  //     const data = await AddToCart(id, count, userId)
-  //     setDisable(false)
-  //     if (data.error) {
-  //       return toast.error("item coun't add to cart")
-  //     } else {
-  //       FetchToCart()
-  //       return toast.success("Item added Successfully")
-  //     }
-  //   } else {
-  //     return toast("You need to login first")
-
-  //   }
-  // }
+  
+  
   return (
     <>
       <div className="product-card" >
