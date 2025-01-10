3a1fea6530f9d20a6578d930827dd065099d2127
AbhijithKK <abhijithkkpurameri@gmail.com>
2025-01-10 16:20:14 +0530

    update token missing error

--- src/Utils/Axios.ts
+++ src/Utils/Axios.ts
@@ -3,20 +3,32 @@ import axios from "axios";
 export const API_BASE_URL = import.meta.env.VITE_APP_API_URL;
 
 const createAxiosInstance = () => {
-    const token = localStorage.getItem('users');
- 
-    
-    const apiToken = token ? JSON.parse(token)['api_token'] : null;
-console.log(apiToken,'token');
+  const instance = axios.create({
+    baseURL: API_BASE_URL,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
 
-    return axios.create({
-        baseURL: API_BASE_URL,
-        headers: {
-            'Content-Type': 'application/json',
-            Authorization: apiToken ? 'Bearer ' + apiToken : null
-        }
-    });
+  // Add a request interceptor
+  instance.interceptors.request.use(
+    (config) => {
+      const token = localStorage.getItem('users');
+      const apiToken = token ? JSON.parse(token)['api_token'] : null;
+
+      if (apiToken) {
+        config.headers['Authorization'] = `Bearer ${apiToken}`;
+      }
+
+      return config;
+    },
+    (error) => {
+      // Handle request errors
+      return Promise.reject(error);
+    }
+  );
+
+  return instance;
 };
-
 
 export { createAxiosInstance };
