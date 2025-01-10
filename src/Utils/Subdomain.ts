e2dd9475effa892a168e8d1ffe2b2ec1dd6dac5a
AbhijithKK <abhijithkkpurameri@gmail.com>
2025-01-10 17:17:38 +0530

    subdomine changed

--- src/Utils/Subdomain.ts
+++ src/Utils/Subdomain.ts
@@ -8,6 +8,6 @@ export function getSubdomain(hostname: string) {
       return parts.slice(0, parts.length - 2).join(".");
     }
       
-    // return null; 
-    return "bba"
+    return null; 
+    // return "bba"
   }
