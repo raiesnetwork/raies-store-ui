ab3b65c28a9d17b740f36d238e1416bfbd67ad58
hafizswabah <swabah.a.avd121@gmail.com>
2025-03-27 14:31:08 +0530

    updated

--- src/Components/Store/Molecules/StockRequestModal.tsx
+++ src/Components/Store/Molecules/StockRequestModal.tsx
@@ -47,9 +47,9 @@ const StockRequestModal: React.FC = () => {
 
     try {
       setLoading(true);
-      const data= await postStockRequestApi(
+      const data = await postStockRequestApi(
         singleProductData?.productName, stockQuantity,
-        expectedDate,advancePayment,singleProductData?._id,singleProductData.user);
+        expectedDate, advancePayment, singleProductData?._id, singleProductData.user);
 
       toast.success(data?.message)
       setOpenBiddingModal()
@@ -86,13 +86,18 @@ const StockRequestModal: React.FC = () => {
             <Form.Label>
               <strong>Number of Stock Requests</strong>
             </Form.Label>
+
             <Form.Control
               type="number"
-              min="1"
-              value={stockQuantity}
-              onChange={(e) => setStockQuantity(Number(e.target.value))}
+              value={isNaN(stockQuantity) ? "" : stockQuantity} // Show empty when NaN
+              onChange={(e) => {
+                const value = e.target.value;
+                setStockQuantity(value === "" ? NaN : Number(value)); // Use NaN for empty case
+              }}
               isInvalid={stockQuantity < 1}
             />
+
+
             <Form.Control.Feedback type="invalid">
               Must be at least 1.
             </Form.Control.Feedback>
--- src/Utils/Subdomain.ts
+++ src/Utils/Subdomain.ts
@@ -7,4 +7,5 @@ export function getSubdomain(hostname: string) {
     }
       
     return null; 
+  
   }
